44f6b17fd44bf406552cafe45eb092a0e8bb9569
CerritoCode0101 <CaseyCerrito@gmail.com>
2020-10-14 18:30:43 -0500

    finishing touches

--- index.js
+++ index.js
@@ -81,51 +81,51 @@ let fred = new Person("fred", 22);
 */
 
 
-function Car(model, milesPerGallon){
-  this.model = model;
-  this.mpg = milesPerGallon;
-  this.tank = 0;
-  this.odometer = 0;
-}
-
-Car.prototype.fill = function(gallons){
-  this.tank = this.tank + gallons;
-}
-
-Car.prototype.drive = function(distance){
-  const drivableMiles
-
-}
-
-
-
-
-
-
-
-
-
-
-// function Car(model, mpg) {
+// function Car(model, milesPerGallon){
 //   this.model = model;
-//   this.milesPerGallon = mpg;
+//   this.mpg = milesPerGallon;
 //   this.tank = 0;
 //   this.odometer = 0;
 // }
-// Car.prototype.fill = function (gallons) {
+
+// Car.prototype.fill = function(gallons){
 //   this.tank = this.tank + gallons;
 // }
-// Car.prototype.drive = function (dist) {
-//   const drivableMiles = this.tank * this.milesPerGallon;
-//   if (dist <= drivableMiles) {
-//     this.odometer = this.odometer + dist;
-//     this.tank = this.tank - (dist / this.milesPerGallon)
-//   } else {
-//     this.odometer = this.odometer + drivableMiles;
-//     this.tank = 0;
-//     return `I ran out of fuel at ${this.odometer} miles!`
-//   }
+
+// Car.prototype.drive = function(distance){
+//   const drivableMiles
+
 // }
+
+
+
+
+
+
+
+
+
+
+function Car(model, mpg) {
+  this.model = model;
+  this.milesPerGallon = mpg;
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function (gallons) {
+  this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function (dist) {
+  const drivableMiles = this.tank * this.milesPerGallon;
+  if (dist <= drivableMiles) {
+    this.odometer = this.odometer + dist;
+    this.tank = this.tank - (dist / this.milesPerGallon)
+  } else {
+    this.odometer = this.odometer + drivableMiles;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
+}
 
 /*
   TASK 3
